b1974294fcafb631800eb958115b19f92408ce8b
Qiang Wang <wang-q@outlook.com>
2024-11-03 16:43:29 +0800

    Multiple lines of the same original_name will also duplicate the record

--- src/cmd/replace.rs
+++ src/cmd/replace.rs
@@ -1,5 +1,6 @@
 use clap::*;
 use noodles_fasta as fasta;
+use std::collections::HashMap;
 
 // Create clap subcommand arguments
 pub fn make_subcommand() -> Command {
@@ -10,8 +11,11 @@ pub fn make_subcommand() -> Command {
 * <replace.tsv> is a tab-separated file containing two or more fields
 
         original_name   replace_name    more_replace_name
+        original_name   replace_name
+        original_name   another_replace_name
 
-    * Three or more fields duplicates the record
+* Three or more fields duplicates the record
+* Multiple lines of the same original_name will also duplicate the record
 
 "###,
         )
@@ -49,7 +53,7 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
     let reader = intspan::reader(args.get_one::<String>("infile").unwrap());
     let mut fa_in = fasta::io::Reader::new(reader);
 
-    let replace_of = intspan::read_replaces(args.get_one::<String>("replace.tsv").unwrap());
+    let replace_of = read_replaces(args.get_one::<String>("replace.tsv").unwrap());
     let is_some = args.get_flag("some");
 
     let writer = intspan::writer(args.get_one::<String>("outfile").unwrap());
@@ -78,4 +82,27 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+fn read_replaces(input: &str) -> HashMap<String, Vec<String>> {
+    let mut replaces: HashMap<String, Vec<String>> = HashMap::new();
+
+    for line in intspan::read_lines(input) {
+        let mut fields: Vec<&str> = line.split('\t').collect();
+
+        let key = fields[0].to_string();
+        let mut left = fields
+            .split_off(1)
+            .iter()
+            .map(|s| (*s).to_string())
+            .collect::<Vec<_>>();
+
+        // Use the entry method to check if the key exists
+        replaces
+            .entry(key.clone())
+            .or_insert_with(Vec::new)
+            .append(&mut left);
+    }
+
+    replaces
 }
