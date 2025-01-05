8bc219318a5a514b681459d3edf12f7b6d4691e2
Qiang Wang <wang-q@outlook.com>
2025-01-05 19:32:44 +0800

    Update jaccard.rs

--- benches/jaccard.rs
+++ benches/jaccard.rs
@@ -10,7 +10,7 @@ use std::collections::{BTreeSet, HashSet};
 
 fn generate_random_set(len: usize, size: usize) -> Vec<Vec<u64>> {
     let mut rng = rand::thread_rng();
-    let side = rand::distributions::Uniform::new(0, u64::MAX);
+    let side = rand::distributions::Uniform::new(0, u16::MAX as u64);
 
     let mut vec = Vec::new();
     for _ in 0..size {
@@ -21,10 +21,11 @@ fn generate_random_set(len: usize, size: usize) -> Vec<Vec<u64>> {
     vec
 }
 
-const LEN: usize = 1005;
+const LEN: usize = 5005;
 const SIZE: usize = 105;
+
 fn btree_inter(c: &mut Criterion) {
-    let vec : Vec<BTreeSet<u64>>= generate_random_set(LEN, SIZE)
+    let vec: Vec<BTreeSet<u64>> = generate_random_set(LEN, SIZE)
         .iter()
         .map(|set| set.iter().cloned().collect())
         .collect();
@@ -35,14 +36,14 @@ fn btree_inter(c: &mut Criterion) {
             let side = rand::distributions::Uniform::new(0, SIZE);
             let set1 = vec.get(rng.sample(side)).unwrap();
             let set2 = vec.get(rng.sample(side)).unwrap();
-            let inter: BTreeSet<_> = set1.intersection(set2).cloned().collect();
+            let inter = set1.intersection(set2).cloned().count();
             black_box(inter);
         })
     });
 }
 
 fn btree_union(c: &mut Criterion) {
-    let vec : Vec<BTreeSet<u64>>= generate_random_set(LEN, SIZE)
+    let vec: Vec<BTreeSet<u64>> = generate_random_set(LEN, SIZE)
         .iter()
         .map(|set| set.iter().cloned().collect())
         .collect();
@@ -53,14 +54,14 @@ fn btree_union(c: &mut Criterion) {
             let side = rand::distributions::Uniform::new(0, SIZE);
             let set1 = vec.get(rng.sample(side)).unwrap();
             let set2 = vec.get(rng.sample(side)).unwrap();
-            let union: BTreeSet<_> = set1.union(set2).cloned().collect();
+            let union = set1.union(set2).cloned().count();
             black_box(union);
         })
     });
 }
 
 fn hashset_inter(c: &mut Criterion) {
-    let vec : Vec<HashSet<u64>>= generate_random_set(LEN, SIZE)
+    let vec: Vec<HashSet<u64>> = generate_random_set(LEN, SIZE)
         .iter()
         .map(|set| set.iter().cloned().collect())
         .collect();
@@ -71,14 +72,14 @@ fn hashset_inter(c: &mut Criterion) {
             let side = rand::distributions::Uniform::new(0, SIZE);
             let set1 = vec.get(rng.sample(side)).unwrap();
             let set2 = vec.get(rng.sample(side)).unwrap();
-            let inter: HashSet<_> = set1.intersection(set2).cloned().collect();
+            let inter = set1.intersection(set2).count();
             black_box(inter);
         })
     });
 }
 
 fn hashset_union(c: &mut Criterion) {
-    let vec : Vec<HashSet<u64>>= generate_random_set(LEN, SIZE)
+    let vec: Vec<HashSet<u64>> = generate_random_set(LEN, SIZE)
         .iter()
         .map(|set| set.iter().cloned().collect())
         .collect();
@@ -89,21 +90,90 @@ fn hashset_union(c: &mut Criterion) {
             let side = rand::distributions::Uniform::new(0, SIZE);
             let set1 = vec.get(rng.sample(side)).unwrap();
             let set2 = vec.get(rng.sample(side)).unwrap();
-            let union: HashSet<_> = set1.union(set2).cloned().collect();
+            let union = set1.union(set2).count();
             black_box(union);
         })
     });
 }
 
-criterion_group!(benches, btree_inter, btree_union, hashset_inter, hashset_union);
+fn btree_jaccard(c: &mut Criterion) {
+    let vec: Vec<BTreeSet<u64>> = generate_random_set(LEN, SIZE)
+        .iter()
+        .map(|set| set.iter().cloned().collect())
+        .collect();
+
+    c.bench_function("btree_jaccard", |b| {
+        b.iter(|| {
+            let mut rng = rand::thread_rng();
+            let side = rand::distributions::Uniform::new(0, SIZE);
+            let set1 = vec.get(rng.sample(side)).unwrap();
+            let set2 = vec.get(rng.sample(side)).unwrap();
+
+            let inter = set1.intersection(set2).cloned().count();
+            let union = set1.union(set2).cloned().count();
+
+            let jaccard = (inter as f64) / (union as f64);
+            black_box(jaccard);
+        })
+    });
+}
+
+fn btree_jaccard2(c: &mut Criterion) {
+    let vec: Vec<BTreeSet<u64>> = generate_random_set(LEN, SIZE)
+        .iter()
+        .map(|set| set.iter().cloned().collect())
+        .collect();
+
+    c.bench_function("btree_jaccard2", |b| {
+        b.iter(|| {
+            let mut rng = rand::thread_rng();
+            let side = rand::distributions::Uniform::new(0, SIZE);
+            let set1 = vec.get(rng.sample(side)).unwrap();
+            let set2 = vec.get(rng.sample(side)).unwrap();
+
+            let inter = set1.intersection(set2).cloned().count();
+            let union = set1.len() + set2.len() + inter;
+
+            let jaccard = (inter as f64) / (union as f64);
+            black_box(jaccard);
+        })
+    });
+}
+
+fn hashset_jaccard2(c: &mut Criterion) {
+    let vec: Vec<BTreeSet<u64>> = generate_random_set(LEN, SIZE)
+        .iter()
+        .map(|set| set.iter().cloned().collect())
+        .collect();
+
+    c.bench_function("hashset_jaccard2", |b| {
+        b.iter(|| {
+            let mut rng = rand::thread_rng();
+            let side = rand::distributions::Uniform::new(0, SIZE);
+            let set1 = vec.get(rng.sample(side)).unwrap();
+            let set2 = vec.get(rng.sample(side)).unwrap();
+
+            let inter = set1.intersection(set2).count();
+            let union = set1.len() + set2.len() + inter;
+
+            let jaccard = (inter as f64) / (union as f64);
+            black_box(jaccard);
+        })
+    });
+}
+
+criterion_group!(
+    benches,
+    btree_inter,
+    btree_union,
+    hashset_inter,
+    hashset_union,
+    btree_jaccard,
+    btree_jaccard2,
+    hashset_jaccard2
+);
 criterion_main!(benches);
 
 // msvc
-// norm_map                time:   [8.9184 µs 8.9294 µs 8.9423 µs]
-// norm_fold               time:   [8.9407 µs 8.9542 µs 8.9664 µs]
-// norm_simd               time:   [1.1196 µs 1.1214 µs 1.1230 µs]
-
-// wsl2
-// norm_map                time:   [8.9086 µs 8.9226 µs 8.9381 µs]
-// norm_fold               time:   [8.9320 µs 8.9456 µs 8.9621 µs]
-// norm_simd               time:   [1.1420 µs 1.1442 µs 1.1470 µs]
+// btree_jaccard2          time:   [44.467 µs 45.254 µs 46.136 µs]
+// hashset_jaccard2        time:   [48.958 µs 50.457 µs 52.138 µs]
