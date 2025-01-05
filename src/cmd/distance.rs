0ffca10d5bedb98b6338f43ce791ce5e5a80ea59
Qiang Wang <wang-q@outlook.com>
2025-01-05 20:39:38 +0800

    Update distance.rs

--- src/cmd/distance.rs
+++ src/cmd/distance.rs
@@ -7,12 +7,13 @@ use std::iter::FromIterator;
 // Create clap subcommand arguments
 pub fn make_subcommand() -> Command {
     Command::new("distance")
-        .about("Estimate distances between DNA/protein sequences")
+        .about("Estimate distances between DNA/protein sequences using minimizers")
         .after_help(
             r###"
-* <infile> can be plain text or bgzf but not stdin or gzip
+This command calculates pairwise distances between sequences in a FA file using minimizers.
+
 * The outputs:
-    n1, n2, mash, jaccard, containment
+    n1  n2  mash    jaccard containment
 
 * Minimizers
     Given a $(k + w - 1)$-mer, consider the $w$ contained $k$-mers. The (rightmost) $k$-mer with
@@ -26,13 +27,29 @@ pub fn make_subcommand() -> Command {
 * To get accurate pairwise sequence identities, use clustalo
   https://lh3.github.io/2018/11/25/on-the-definition-of-sequence-identity
 
+Examples:
+    1. Calculate distances with default parameters:
+       hnsm distance input.fa
+
+    2. Use MurmurHash instead of FxHash:
+       hnsm distance input.fa --hasher murmur
+
+    3. Set custom k-mer size and window size:
+       hnsm distance input.fa -k 21 -w 5
+
+    4. Convert distance to similarity:
+       hnsm distance input.fa --sim
+
+    5. Use 4 threads for parallel processing:
+       hnsm distance input.fa --parallel 4
+
 "###,
         )
         .arg(
             Arg::new("infile")
                 .required(true)
                 .index(1)
-                .help("Set the input file to use"),
+                .help("Input FA file to process"),
         )
         .arg(
             Arg::new("hasher")
@@ -43,7 +60,7 @@ pub fn make_subcommand() -> Command {
                     builder::PossibleValue::new("murmur"),
                 ])
                 .default_value("fx")
-                .help("Set the hash algorithm"),
+                .help("Hash algorithm to use"),
         )
         .arg(
             Arg::new("kmer")
@@ -52,7 +69,7 @@ pub fn make_subcommand() -> Command {
                 .num_args(1)
                 .default_value("7")
                 .value_parser(value_parser!(usize))
-                .help("Kmer size"),
+                .help("K-mer size"),
         )
         .arg(
             Arg::new("window")
@@ -61,7 +78,7 @@ pub fn make_subcommand() -> Command {
                 .num_args(1)
                 .default_value("1")
                 .value_parser(value_parser!(usize))
-                .help("Window size"),
+                .help("Window size for minimizers"),
         )
         .arg(
             Arg::new("sim")
@@ -75,7 +92,7 @@ pub fn make_subcommand() -> Command {
                 .num_args(1)
                 .default_value("1")
                 .value_parser(value_parser!(usize))
-                .help("Number of threads"),
+                .help("Number of threads for parallel processing"),
         )
         .arg(
             Arg::new("outfile")
@@ -100,13 +117,15 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
     let opt_window = *args.get_one::<usize>("window").unwrap();
     let is_sim = args.get_flag("sim");
 
+    let opt_parallel = *args.get_one::<usize>("parallel").unwrap();
+
     let mut writer = intspan::writer(args.get_one::<String>("outfile").unwrap());
 
     //----------------------------
     // Ops
     //----------------------------
     let mut set_of = HashMap::new();
-    let mut names = vec![];
+    let mut names = vec![]; // track original orders of names
 
     for result in fa_in.records() {
         // obtain record or fail with error
@@ -131,27 +150,15 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
             _ => unreachable!(),
         };
 
-        let set: HashSet<u64> = HashSet::from_iter(minimizers.iter().map(|t| t.1));
         names.push(name.clone());
+        let set: HashSet<u64> = HashSet::from_iter(minimizers.iter().map(|t| t.1));
         set_of.insert(name, set);
     }
-    // eprintln!("set_of = {:#?}", set_of);
 
     for n1 in &names {
         for n2 in &names {
-            let s1 = set_of.get(n1).unwrap();
-            let s2 = set_of.get(n2).unwrap();
-            let inter: HashSet<_> = s1.intersection(&s2).collect();
-            let union: HashSet<_> = s1.union(&s2).collect();
-
-            let jaccard = (inter.len() as f64) / (union.len() as f64);
-            let containment = (inter.len() as f64) / (s1.len() as f64);
-            // https://mash.readthedocs.io/en/latest/distances.html#mash-distance-formulation
-            let mash: f64 = if jaccard == 0.0 {
-                1.0
-            } else {
-                ((-1.0 / 7.0f64) * ((2.0 * jaccard) / (1.0f64 + jaccard)).ln()).abs()
-            };
+            let (jaccard, containment, mash) =
+                calc_distances(set_of.get(n1).unwrap(), set_of.get(n2).unwrap());
 
             writer.write_fmt(format_args!(
                 "{}\t{}\t{:.4}\t{:.4}\t{:.4}\n",
@@ -165,4 +172,21 @@ pub fn execute(args: &ArgMatches) -> anyhow::Result<()> {
     }
 
     Ok(())
+}
+
+// Calculate Jaccard, Containment, and Mash distance between two HashSets
+fn calc_distances(s1: &HashSet<u64>, s2: &HashSet<u64>) -> (f64, f64, f64) {
+    let inter = s1.intersection(&s2).count();
+    let union = s1.len() + s2.len() - inter;
+
+    let jaccard = inter as f64 / union as f64;
+    let containment = inter as f64 / s1.len() as f64;
+    // https://mash.readthedocs.io/en/latest/distances.html#mash-distance-formulation
+    let mash = if jaccard == 0.0 {
+        1.0
+    } else {
+        ((-1.0 / 7.0) * ((2.0 * jaccard) / (1.0 + jaccard)).ln()).abs()
+    };
+
+    (jaccard, containment, mash)
 }
