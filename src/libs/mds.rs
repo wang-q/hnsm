e92ae3c297f9ad974dfb1b79019642e261cc3c02
Qiang Wang <wang-q@outlook.com>
2025-01-09 08:11:39 +0800

    tired

--- src/libs/mds.rs
+++ src/libs/mds.rs
@@ -40,10 +40,7 @@ fit <- cmdscale(matrix, k = 2, eig = TRUE, x.ret = TRUE)
 fit$x
 
 */
-//
-// use ndarray::{Array2, Axis, Zip};
-// use ndarray_linalg::{Eigh, UPLO};
-//
+
 // pub struct Mds {
 //     dim: usize,
 // }
@@ -53,23 +50,34 @@ fit$x
 //         Mds { dim }
 //     }
 //
-//     pub fn double_centering(&self, matrix: &mut Array2<f32>) {
-//         matrix.mapv_inplace(|v| -(v * v) / 2.);
+//     pub fn double_centering(&self, matrix: &faer::Mat<f64>) -> faer::Mat<f64> {
+//         let mut centered = matrix.clone();
 //
-//         let row_means = matrix.mean_axis(Axis(0)).unwrap();
-//         let col_means = matrix.mean_axis(Axis(1)).unwrap();
-//         let matrix_mean = matrix.mean().unwrap();
+//         let ncol = centered.ncols();
+//         let mut col_mean = faer::Row::zeros(ncol);
+//         faer::stats::row_mean(
+//             col_mean.as_mut(),
+//             centered.as_ref(),
+//             faer::stats::NanHandling::Ignore,
+//         );
 //
-//         Zip::indexed(matrix).for_each(|(row, col), val| {
-//             *val = *val - row_means[col] - col_means[row] + matrix_mean
-//         });
-//     }
+//         let nrow = centered.nrows();
+//         let mut row_mean = faer::Row::zeros(nrow);
+//         faer::stats::row_mean(
+//             row_mean.as_mut(),
+//             centered.as_ref(),
+//             faer::stats::NanHandling::Ignore,
+//         );
 //
-//     pub fn eigen(&self, matrix: &mut Array2<f32>) {
-//         let (eigvals, eigvecs) = matrix.eigh(UPLO::Lower)?;
+//         let grand_mean = centered.mean();
 //
-//         eprintln!("eigvals = {:#?}", eigvals);
+//         for j in 0..ncol {
+//             for i in 0..nrow {
+//                 centered[(i, j)] -= row_mean[i] + col_mean[j] - grand_mean;
+//             }
+//         }
 //
+//         centered
 //     }
 // }
 //
@@ -78,27 +86,24 @@ fit$x
 //     use super::*;
 //
 //     #[test]
-//     fn centered() {
-//         let mut matrix: Array2<f32> = ndarray::arr2(&[
+//     fn test_centered() {
+//         let mut matrix = faer::mat![
 //             [0., 7., 5., 5.],
 //             [7., 0., 4., 9.],
 //             [5., 4., 0., 3.],
 //             [5., 9., 3., 0.],
-//         ]);
-//         let exp: Vec<f32> = vec![
-//             11.9375, -6.6875, -6.6875, 1.4375, //
-//             -6.6875, 23.6875, 3.6875, -20.6875, //
-//             -6.6875, 3.6875, -0.3125, 3.3125, //
-//             1.4375, -20.6875, 3.3125, 15.9375, //
+//         ];
+//         let exp: faer::mat![
+//             [11.9375, -6.6875, -6.6875, 1.4375],
+//             [-6.6875, 23.6875, 3.6875, -20.6875],
+//             [-6.6875, 3.6875, -0.3125, 3.3125],
+//             [1.4375, -20.6875, 3.3125, 15.9375],
 //         ];
 //
 //         eprintln!("matrix = {:#?}", matrix);
 //         let mut mds = Mds::new(2);
-//         mds.double_centering(&mut matrix);
+//         let res = mds.double_centering(&mut matrix);
 //         eprintln!("matrix = {:#?}", matrix);
-//
-//         let res = matrix.iter().cloned().collect::<Vec<f32>>();
-//
 //         assert_eq!(res, exp);
 //     }
 // }
