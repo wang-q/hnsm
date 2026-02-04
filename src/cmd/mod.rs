a7182fa8f3111287270304bbcf3190cfd654f43a
Qiang Wang <wang-q@outlook.com>
2026-02-04 22:08:44 +0800

    Moved to pgr

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,17 +1,11 @@
 //! Subcommand modules for the `hnsm` binary.
 
 pub mod clust;
-pub mod dedup;
 pub mod dist;
-pub mod filter;
-pub mod fq2fa;
 pub mod gff;
-pub mod gz;
 pub mod interleave;
 pub mod manifold;
-pub mod mask;
 pub mod mat;
 pub mod prefilter;
-pub mod range;
 pub mod sixframe;
 pub mod synt;
--- src/hnsm.rs
+++ src/hnsm.rs
@@ -14,17 +14,12 @@ fn main() -> anyhow::Result<()> {
         // info
         // records
         // transform
-        .subcommand(cmd::filter::make_subcommand())
-        .subcommand(cmd::dedup::make_subcommand())
-        .subcommand(cmd::mask::make_subcommand())
+        // transform
         .subcommand(cmd::sixframe::make_subcommand())
         // index
-        .subcommand(cmd::gz::make_subcommand())
-        .subcommand(cmd::range::make_subcommand())
         .subcommand(cmd::prefilter::make_subcommand())
         // fastq
         .subcommand(cmd::interleave::make_subcommand())
-        .subcommand(cmd::fq2fa::make_subcommand())
         // distance
         .subcommand(cmd::dist::make_subcommand())
         .subcommand(cmd::manifold::make_subcommand())
@@ -65,16 +60,10 @@ fn main() -> anyhow::Result<()> {
         Some(("gff", sub_matches)) => cmd::gff::execute(sub_matches),
         Some(("mat", sub_matches)) => cmd::mat::execute(sub_matches),
         Some(("clust", sub_matches)) => cmd::clust::execute(sub_matches),
-        Some(("dedup", sub_matches)) => cmd::dedup::execute(sub_matches),
-        Some(("filter", sub_matches)) => cmd::filter::execute(sub_matches),
         Some(("manifold", sub_matches)) => cmd::manifold::execute(sub_matches),
-        Some(("mask", sub_matches)) => cmd::mask::execute(sub_matches),
         Some(("sixframe", sub_matches)) => cmd::sixframe::execute(sub_matches),
-        Some(("gz", sub_matches)) => cmd::gz::execute(sub_matches),
-        Some(("range", sub_matches)) => cmd::range::execute(sub_matches),
         Some(("prefilter", sub_matches)) => cmd::prefilter::execute(sub_matches),
         Some(("interleave", sub_matches)) => cmd::interleave::execute(sub_matches),
-        Some(("fq2fa", sub_matches)) => cmd::fq2fa::execute(sub_matches),
         Some(("dist", sub_matches)) => cmd::dist::execute(sub_matches),
         _ => unreachable!(),
     }?;
